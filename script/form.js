8aa9474be7bde91aa27e72c2e2940faf395cea3f
Romain Rivière <rriviere.cpi2019@esaip.org>
2017-11-23 02:28:41 +0100

    js

--- script/form.js
+++ script/form.js
@@ -1,10 +1,5 @@
-$(function() {
-  $('#submit').click(function() {
-      $.getJSON('data/data.json', function(data) {
-      $('#r').html('<p><b>Speciality</b> : ' + data.specialty + '</p>');
-      $('#r').append('<p><b>Name</b> : ' + data.first_name + '</p>');
-      $('#r').append('<p><b>Surname</b> : ' + data.last_name + '</p>');
-      $('#r').append('<p><b>City</b> : ' + data.city + '</p>');
-    });
+$('#submit').click(function () {
+  $.getJSON('data/data.json', function (data) {
+    dump(data);
   });
 });
