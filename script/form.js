f47d75a2769657cbadce2cf8cdbab15117b5780f
Romain Rivière <rriviere.cpi2019@esaip.org>
2017-11-23 12:55:55 +0100

    js

--- script/form.js
+++ script/form.js
@@ -17,7 +17,7 @@ $(function(){
       );
       /* loop through array */
       $.each(data, function(index, d){
-        console.log(d);
+        console.log(d.specialty);
         html.push(
              "<tr>"+
                 "<td>" + d.specialty + "</td>"+
