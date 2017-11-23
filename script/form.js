804b7d9e80b40637f846484108140ccd1450e6f3
Romain Rivière <rriviere.cpi2019@esaip.org>
2017-11-23 03:21:32 +0100

    js

--- script/form.js
+++ script/form.js
@@ -1,21 +1,20 @@
 $("#submit").click(function(){
-        /* set no cache */
-        $.ajaxSetup({ cache: false });
+  $("#r").html("submited");
+  /* set no cache */
+  $.ajaxSetup({ cache: false });
 
-        $.getJSON("/data/data.json", function(data){
-            var html = [];
-
-            /* loop through array */
-            $.each(data, function(index, d){
-                html.push("Specialty : ", d.specialty, ", ",
-                          "Name : ", d.first_name, ", ",
-                          "Last Name : ", d.last_name, "<br>");
-            });
-
-
-            $("#r").html(html.join('')).css("background-color", "orange");
-        }).error(function(jqXHR, textStatus, errorThrown){ /* assign handler */
-            /* alert(jqXHR.responseText) */
-            alert("error occurred!");
-        });
+  $.getJSON("/data/data.json", function(data){
+    var html = [];
+    /* loop through array */
+    $.each(data, function(index, d){
+      html.push("Specialty : ", d.specialty, ", ",
+                "Name : ", d.first_name, ", ",
+                "Last Name : ", d.last_name, "<br>");
     });
+
+    $("#r").html(html.join('')).css("background-color", "orange");
+  }).error(function(jqXHR, textStatus, errorThrown){ /* assign handler */
+    /* alert(jqXHR.responseText) */
+    alert("error occurred!");
+  });
+});
